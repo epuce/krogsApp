58ebbe19d2da4bfdbc2a727c9e244b093b1d4b52
Edmunds <emis117@inbox.lv>
2017-05-31 22:44:23 +0300

    remove secretr key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authorize
+  before_action :show_nav
 
-  def index
+  def show_nav
     @groups = Group.all
   end
 
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,6 @@ class MainController < ApplicationController
 
   def index
     @maintexts = Maintext.all
-    @groups = Group.all
     @foods = Food.all
     @pictures = Picture.all
   end
