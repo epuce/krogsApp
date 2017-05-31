28d431222390131c709b33e2d99a3ede47362bc8
Edmunds <emis117@inbox.lv>
2017-05-31 23:07:10 +0300

    remove secretr key

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,4 @@
 class Picture < ActiveRecord::Base
-  has_attached_file :name, path: 'app/assets/images/gallery/:filename', url: 'gallery/:filename', validate_media_type: false
+  has_attached_file :name, path: 'app/assets/images/:filename', url: ':filename', validate_media_type: false
   validates_attachment_content_type :name, content_type: /\Aimage\/.*\z/
 end
