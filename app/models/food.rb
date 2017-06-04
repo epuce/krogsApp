341bbe2f7b343ed9f182c5907f0972afb6036848
Edmunds <emis117@inbox.lv>
2017-06-04 17:06:09 +0300

    uzliku order loģiku

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ActiveRecord::Base
-  validates :name, :price, :group_id, :order, presence: {message: "lauks nedrīkst būt tukšs"}
+  validates :name, :price, :group_id, presence: {message: "lauks nedrīkst būt tukšs"}
   validates :name, uniqueness: {message: "ieraksts ar šādu nosaukumu jau eksistē"}
   validates :order, uniqueness: {message: "ieraksts ar šādu secības numerāciju jau eksistē"}
   validates :price, numericality: {greater_than_or_equal_to: 0.05, message: "laukā, skaitlim jābūt pozitīvam"}
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,5 @@
 class Picture < ActiveRecord::Base
   has_attached_file :name, path: 'public/gallery/:filename', url: '/gallery/:filename', validate_media_type: false
   validates_attachment_content_type :name, content_type: /\Aimage\/.*\z/
-  validates :order, presence: {message: "lauks nedrīkst būt tukšs"}
   validates :order, uniqueness: {message: "ieraksts ar šādu secības numerāciju jau eksistē"}
 end
