dad45595626e15b35f98d056ee476b3dccf3d4af
epuce <edmunds.puce@inbox.lv>
2017-12-10 13:28:13 +0200

    Delete 20171203104324_change_colum_names_add_group_to_food.rb

--- db/migrate/20171203104324_change_colum_names_add_group_to_food.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumNamesAddGroupToFood < ActiveRecord::Migration[5.0]
-  def change
-	  add_column :foods, :group_id, :integer
-	  rename_column :groups, :text, :name
-	  rename_column :subgroups, :subgroup, :name
-  end
-end
